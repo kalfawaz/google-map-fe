a8a916e91ebfa4d37a113a21e54e82d77d8e2b1f
kalfawaz@tahakom.com <kalfawaz@tahakom.com>
2025-02-26 15:51:52 +0300

    setup language

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
+
+const provideTranslation = () => ({
+  loader: {
+    provide: TranslateLoader,
+    useFactory: httpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      BrowserAnimationsModule,
+      BrowserModule,
+      TranslateModule.forRoot(provideTranslation()),
+    ]),
+    provideRouter(routes, withViewTransitions()),
+  ],
 };
