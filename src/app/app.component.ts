16994fd83973cf34340279751fabf57a7b93c2db
kalfawaz@tahakom.com <kalfawaz@tahakom.com>
2025-02-27 10:56:06 +0300

    first setup language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,15 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    TranslateModule
-  ],
+  imports: [RouterOutlet, CommonModule, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'google-map-fe';
+
+  constructor(private translate: TranslateService) {
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang || 'en');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
@@ -7,7 +11,9 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 const provideTranslation = () => ({
   loader: {
@@ -20,12 +26,12 @@ const provideTranslation = () => ({
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withFetch()),
     importProvidersFrom([
       BrowserAnimationsModule,
       BrowserModule,
       TranslateModule.forRoot(provideTranslation()),
     ]),
-    provideRouter(routes, withViewTransitions()),
   ],
 };
