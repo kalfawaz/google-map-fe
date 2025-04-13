0787b30aa00563960f7305b1b3477137662f79fa
kalfawaz@tahakom.com <kalfawaz@tahakom.com>
2025-04-13 11:10:43 +0300

    custom button added

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+  @Input() label!: string;
+  // @Input() imgStyle!: string;
+  // @Input() icon!: string;
+  @Input() isDisabled!: boolean;
+  @Input() buttonStyle!: string;
+  @Output() triggerFunc = new EventEmitter();
 }
